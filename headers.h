215b24b54498028afd4b7bef9f4bc837411de910
VictoriaSzy <vszymanowski00@stuy.edu>
2019-10-23 19:56:17 -0400

    Added correct parameters to fucntions

--- headers.h
+++ headers.h
@@ -1,2 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+struct node {
+  int i ;
+  struct node * next ;
+} ;
+void print_list(struct node * l) ;
+struct node * insert_front(struct node * l, int x) ;
+struct node * free_list(struct node * l) ;
