03703aff476bd45a6ac44cbbc6317a89ec830c21
VictoriaSzy <vszymanowski00@stuy.edu>
2019-10-23 19:56:36 -0400

    Added directions

--- main.c
+++ main.c
@@ -3,3 +3,10 @@
 int main() {
   return 0 ;
 }
+
+/*
+Write a simple linked list program. You will need a define a node struct with the following data members:
+- A piece of data (your choice)
+- A pointer to the next node
+- Here's an example of what the struct could look like: struct node { int i; struct node *next; };
+*/
