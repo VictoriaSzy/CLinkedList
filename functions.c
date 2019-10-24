2781e6f29882cd8806a55160797c931d1f6a81a5
VictoriaSzy <vszymanowski00@stuy.edu>
2019-10-24 13:45:54 -0400

    Corrected printing empty list

--- functions.c
+++ functions.c
@@ -2,10 +2,14 @@
 
 void print_list(struct node * l) {
   //Should take a pointer to a node struct and print out all of the data in the list
-  if ( l -> next ) {
-    printf("%d ", l -> i) ; // use i since that's the int we have as part of our struct
-    print_list( l -> i ) ;
+  printf("[") ;
+  if (l->next) {
+    printf("%d", l->i) ;
   }
+  while (l = l->next) {
+    printf(", %d", l->i) ; // use i since that's the int we have as part of our struct
+  }
+  printf("]\n") ;
 }
 
 struct node * insert_front(struct node * l, int x) {
@@ -15,18 +19,25 @@ struct node * insert_front(struct node * l, int x) {
   Returns a pointer to the beginning of the list.*/
   struct node * a = calloc(1, sizeof(struct node)) ; // use calloc since it returns a pointer and that's how we want to start
   free(a) ; // release the beginning because we want a new beginning
-  a -> i = x ;
-  a -> next = l ;
+  a->i = x ;
+  a->next = l ;
   return a ;
 }
 
 struct node * free_list(struct node * l) {
   // Should take a pointer to a list as a parameter and then go through
   // the entire list freeing each node and return a pointer to the beginning of the list (which should be NULL by then).
-  if (l -> next) free_list(l -> next) ;
+  if (l->next) free_list(l->next) ;
   else {
     struct node * a = l ;
     free(l) ; // free or release the original node given or the copy that was passed into this function
     return a ; // return the one we just made
   }
+}
+
+struct node * remove(struct node *front, int data) {
+  /*Remove the node containing data from the list pointed to by front.
+  If data is not in the list, nothing is changed.
+  Returns a pointer to the beginning of the list.*/
+
 }
