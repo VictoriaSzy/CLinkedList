ae3e28bd12947e3d6c4ab60fa0b308966d94568f
VictoriaSzy <vszymanowski00@stuy.edu>
2019-10-23 20:02:22 -0400

    Added print list function by using recursion!

--- functions.c
+++ functions.c
@@ -2,16 +2,19 @@
 
 void print_list(struct node * l) {
   //Should take a pointer to a node struct and print out all of the data in the list
+  if ( l -> next ) {
+    printf("%d ", l -> i) ; // use i since that's the int we have as part of our struct
+    print_list( l -> i ) ;
+  }
 }
 
 struct node * insert_front(struct node * l, int x) {
-  /*
-  Should take a pointer to the existing list and the data to be added,
+  /*Should take a pointer to the existing list and the data to be added,
   create a new node and put it at the beginning of the list.
   The second argument should match whatever data you contain in your nodes.
-  Returns a pointer to the beginning of the list.
-  */
+  Returns a pointer to the beginning of the list.*/
 }
+
 struct node * free_list(struct node * l) {
   // Should take a pointer to a list as a parameter and then go through
   // the entire list freeing each node and return a pointer to the beginning of the list (which should be NULL by then).
