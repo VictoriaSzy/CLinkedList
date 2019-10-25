5fa9381fffab7a7347ce4bec9c522df01c289d45
VictoriaSzy <vszymanowski00@stuy.edu>
2019-10-25 11:56:54 -0400

    Began testing

--- main.c
+++ main.c
@@ -2,8 +2,18 @@
 
 int main() {
   struct node * q = calloc(1, sizeof(struct node)) ;
-  printf("Printing empty list:\n") ;
+  printf("Printing empty list: \n") ;
   print_list(q) ;
+  printf("Adding #s 0-9 to list.") ;
+  int z ;
+  for (z = 0 ; z < 9 ; z++) {
+    insert_front(q, z) ;
+  }
+  printf("Printing my new list that has values added: \n") ;
+  print_list(q) ;
+  printf("And now we're going to free nodes in order:\n") ;
+  free_list(q) ;
+  printf("The list will now be freed\n") ;
   return 0 ;
 }
 
